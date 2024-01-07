7d0ff22889f7ef4332ce0c6c557fc8760b9c38ba
linxun1209 <3056726345@qq.com>
2024-01-07 11:34:07 +0800

    1.初始化前端模板 实现功能 用户 注册，登录，退出 管理员 新增，修改,查询，删除 用户

---
+++ src/services/backend/typings.d.ts
@@ -0,0 +1,297 @@
+declare namespace API {
+  type BaseResponseboolean = {
+    code?: number;
+    data?: boolean;
+    message?: string;
+  };
+
+  type BaseResponseint = {
+    code?: number;
+    data?: number;
+    message?: string;
+  };
+
+  type BaseResponseLoginUserVO = {
+    code?: number;
+    data?: LoginUserVO;
+    message?: string;
+  };
+
+  type BaseResponselong = {
+    code?: number;
+    data?: string;
+    message?: string;
+  };
+
+  type BaseResponsePagePostVO = {
+    code?: number;
+    data?: PagePostVO;
+    message?: string;
+  };
+
+  type BaseResponsePageUser = {
+    code?: number;
+    data?: PageUser;
+    message?: string;
+  };
+
+  type BaseResponsePageUserVO = {
+    code?: number;
+    data?: PageUserVO;
+    message?: string;
+  };
+
+  type BaseResponsePostVO = {
+    code?: number;
+    data?: PostVO;
+    message?: string;
+  };
+
+  type BaseResponsestring = {
+    code?: number;
+    data?: string;
+    message?: string;
+  };
+
+  type BaseResponseUser = {
+    code?: number;
+    data?: User;
+    message?: string;
+  };
+
+  type BaseResponseUserVO = {
+    code?: number;
+    data?: UserVO;
+    message?: string;
+  };
+
+  type checkUsingGETParams = {
+    /** timestamp */
+    timestamp?: string;
+    /** nonce */
+    nonce?: string;
+    /** signature */
+    signature?: string;
+    /** echostr */
+    echostr?: string;
+  };
+
+  type DeleteRequest = {
+    id?: string;
+  };
+
+  type getPostVOByIdUsingGETParams = {
+    /** id */
+    id?: string;
+  };
+
+  type getUserByIdUsingGETParams = {
+    /** id */
+    id?: string;
+  };
+
+  type getUserVOByIdUsingGETParams = {
+    /** id */
+    id?: string;
+  };
+
+  type LoginUserVO = {
+    createTime?: string;
+    id?: string;
+    updateTime?: string;
+    userAvatar?: string;
+    userName?: string;
+    userProfile?: string;
+    userRole?: string;
+  };
+
+  type OrderItem = {
+    asc?: boolean;
+    column?: string;
+  };
+
+  type PagePostVO = {
+    countId?: string;
+    current?: string;
+    maxLimit?: string;
+    optimizeCountSql?: boolean;
+    orders?: OrderItem[];
+    pages?: string;
+    records?: PostVO[];
+    searchCount?: boolean;
+    size?: string;
+    total?: string;
+  };
+
+  type PageUser = {
+    countId?: string;
+    current?: string;
+    maxLimit?: string;
+    optimizeCountSql?: boolean;
+    orders?: OrderItem[];
+    pages?: string;
+    records?: User[];
+    searchCount?: boolean;
+    size?: string;
+    total?: string;
+  };
+
+  type PageUserVO = {
+    countId?: string;
+    current?: string;
+    maxLimit?: string;
+    optimizeCountSql?: boolean;
+    orders?: OrderItem[];
+    pages?: string;
+    records?: UserVO[];
+    searchCount?: boolean;
+    size?: string;
+    total?: string;
+  };
+
+  type PostAddRequest = {
+    content?: string;
+    tags?: string[];
+    title?: string;
+  };
+
+  type PostEditRequest = {
+    content?: string;
+    id?: string;
+    tags?: string[];
+    title?: string;
+  };
+
+  type PostFavourAddRequest = {
+    postId?: string;
+  };
+
+  type PostFavourQueryRequest = {
+    current?: string;
+    pageSize?: string;
+    postQueryRequest?: PostQueryRequest;
+    sortField?: string;
+    sortOrder?: string;
+    userId?: string;
+  };
+
+  type PostQueryRequest = {
+    content?: string;
+    current?: string;
+    favourUserId?: string;
+    id?: string;
+    notId?: string;
+    orTags?: string[];
+    pageSize?: string;
+    searchText?: string;
+    sortField?: string;
+    sortOrder?: string;
+    tags?: string[];
+    title?: string;
+    userId?: string;
+  };
+
+  type PostThumbAddRequest = {
+    postId?: string;
+  };
+
+  type PostUpdateRequest = {
+    content?: string;
+    id?: string;
+    tags?: string[];
+    title?: string;
+  };
+
+  type PostVO = {
+    content?: string;
+    createTime?: string;
+    favourNum?: number;
+    hasFavour?: boolean;
+    hasThumb?: boolean;
+    id?: string;
+    tagList?: string[];
+    thumbNum?: number;
+    title?: string;
+    updateTime?: string;
+    user?: UserVO;
+    userId?: string;
+  };
+
+  type uploadFileUsingPOSTParams = {
+    biz?: string;
+  };
+
+  type User = {
+    createTime?: string;
+    id?: string;
+    isDelete?: number;
+    mpOpenId?: string;
+    unionId?: string;
+    updateTime?: string;
+    userAccount?: string;
+    userAvatar?: string;
+    userName?: string;
+    userPassword?: string;
+    userProfile?: string;
+    userRole?: string;
+  };
+
+  type UserAddRequest = {
+    userAccount?: string;
+    userAvatar?: string;
+    userName?: string;
+    userRole?: string;
+  };
+
+  type userLoginByWxOpenUsingGETParams = {
+    /** code */
+    code: string;
+  };
+
+  type UserLoginRequest = {
+    userAccount?: string;
+    userPassword?: string;
+  };
+
+  type UserQueryRequest = {
+    current?: string;
+    id?: string;
+    mpOpenId?: string;
+    pageSize?: string;
+    sortField?: string;
+    sortOrder?: string;
+    unionId?: string;
+    userName?: string;
+    userProfile?: string;
+    userRole?: string;
+  };
+
+  type UserRegisterRequest = {
+    checkPassword?: string;
+    userAccount?: string;
+    userPassword?: string;
+  };
+
+  type UserUpdateMyRequest = {
+    userAvatar?: string;
+    userName?: string;
+    userProfile?: string;
+  };
+
+  type UserUpdateRequest = {
+    id?: string;
+    userAvatar?: string;
+    userName?: string;
+    userProfile?: string;
+    userRole?: string;
+  };
+
+  type UserVO = {
+    createTime?: string;
+    id?: string;
+    userAvatar?: string;
+    userName?: string;
+    userProfile?: string;
+    userRole?: string;
+  };
+}
